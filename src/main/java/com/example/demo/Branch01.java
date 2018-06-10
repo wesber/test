fc16359986489b479a9feb04a45e66c4c265640d
Administrator <Administrator@P3JUC5GQL0MO45R>
2018-06-10 22:12:22 +0800

    撒地方都是

--- src/main/java/com/example/demo/Branch01.java
+++ src/main/java/com/example/demo/Branch01.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class Branch01 {
 	public static void main(String[] args) {
-		long currentTimeMillis = System.currentTimeMillis();
+		System.out.println("aaaa");
 	}
 }

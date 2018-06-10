18c802db24afe27e12c2c47f1f95fade95e874a5
Administrator <Administrator@P3JUC5GQL0MO45R>
2018-06-10 22:04:15 +0800

    dsaf

--- src/main/java/com/example/demo/Branch01.java
+++ src/main/java/com/example/demo/Branch01.java
@@ -2,8 +2,8 @@ package com.example.demo;
 
 public class Branch01 {
 	public static void main(String[] args) {
-		System.out.println("qqq66");
-		System.out.println("qqq777777777");
+		System.out.println("qqq66333");
+		System.out.println("www333");
 		
 	}
 }
